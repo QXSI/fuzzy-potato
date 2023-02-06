ee74709b8d155b896a3c4f662c27fd86bff82dbb
QXSI <124586680+QXSI@users.noreply.github.com>
2023-02-06 14:25:23 +0100

    Delete 05_Videogiochi directory

--- 05_Videogiochi/src/main/java/it/cefi/repositories/SviluppatoriRepository.java
+++
@@ -1,9 +0,0 @@
-package it.cefi.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import it.cefi.models.Sviluppatore;
-
-public interface SviluppatoriRepository extends CrudRepository<Sviluppatore, Integer> {
-
-}
